beed98c1358b7e75b2e6f7cf3fe0b4690b030bbe
reidr <reidr@DESKTOP-659UTJP>
2022-06-03 13:30:17 +0100

    Failing test for retunring single digit.

--- String Calculator Kata 03062022/StringCalc_Tests.cs
+++ String Calculator Kata 03062022/StringCalc_Tests.cs
@@ -5,11 +5,21 @@ namespace StringCalc
     public class StringcalcAdd
     {
         [Test]
-        public void StringCal_Add_EmptyReturns0()
+        public void StringCalc_Add_EmptyReturns0()
         {
             var stringCalc = new StringCalc();
             var result = stringCalc.add("");
             Assert.AreEqual(0, result); 
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        public void StringCalc_Add_SingleIntReturnsInt(string inp, int Expected)
+        {
+            var stringCalc = new StringCalc();
+            var result = stringCalc.add(inp);
+            Assert.AreEqual(Expected, result);
         }
 
     }
